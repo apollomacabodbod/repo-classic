e043f68777f7b137fae7557fc78453717066988a
apollo <apollomacabodbod11@gmail.com>
2024-08-07 13:54:08 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Form from 'components/ui/kit/Form'
+
+export default function Home() {
+  return (
+
+    <>
+
+<div className="flex flex-col">
+
+  <div className="flex items-center justify-center">
+    
+  <Form/>
+
+  </div>
+</div>
+   
+
+
+    
+    </>
+  );
+}
